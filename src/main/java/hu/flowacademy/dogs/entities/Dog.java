846d7ab306ebb65633437600babf1aa8595b528c
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-17 11:38:53 +0200

    Add JsonIgnore to Dog entity

--- src/main/java/hu/flowacademy/dogs/entities/Dog.java
+++ src/main/java/hu/flowacademy/dogs/entities/Dog.java
@@ -1,5 +1,6 @@
 package hu.flowacademy.dogs.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -23,5 +24,6 @@ public class Dog {
 
     @ManyToOne
     @JoinColumn
+    @JsonIgnore
     private Shelter shelter;
 }
