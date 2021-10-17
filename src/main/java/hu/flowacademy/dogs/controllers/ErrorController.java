71f191e5b73fa3f4b38b70d67a743152be162956
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-17 07:55:24 +0200

    Add notValidArgument method

--- src/main/java/hu/flowacademy/dogs/controllers/ErrorController.java
+++ src/main/java/hu/flowacademy/dogs/controllers/ErrorController.java
@@ -1,8 +1,18 @@
 package hu.flowacademy.dogs.controllers;
 
+import hu.flowacademy.dogs.models.ErrorModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ErrorController {
 
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorModel notValidArgument() {
+        return new ErrorModel("Érvénytelen bemenet!");
+    }
 }
