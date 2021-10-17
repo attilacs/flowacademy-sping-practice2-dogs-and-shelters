67ad4cfb381f5e636eb7f731bf651fea17b92103
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-17 10:12:52 +0200

    Add addDog method to DogService

--- src/main/java/hu/flowacademy/dogs/services/DogService.java
+++ src/main/java/hu/flowacademy/dogs/services/DogService.java
@@ -1,5 +1,8 @@
 package hu.flowacademy.dogs.services;
 
+import hu.flowacademy.dogs.dtos.DogDTO;
+import hu.flowacademy.dogs.entities.Dog;
+import hu.flowacademy.dogs.entities.Shelter;
 import hu.flowacademy.dogs.repositories.DogRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +11,15 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class DogService {
     private final DogRepository dogRepository;
+    private final ShelterService shelterService;
+
+    public Dog addDog(DogDTO dogDTO) {
+        Shelter shelter = shelterService.findShelterById(dogDTO.getShelter_id());
+        Dog dog = new Dog();
+        dog.setChipId(dogDTO.getChipId());
+        dog.setName(dogDTO.getName());
+        dog.setGender(dogDTO.getGender());
+        dog.setShelter(shelter);
+        return dog;
+    }
 }
