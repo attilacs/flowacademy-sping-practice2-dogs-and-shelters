fa690904f08e4a1f62024282f3eccb0d155a3c72
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-17 05:14:07 +0200

    Initial commit

---
+++ src/test/java/hu/flowacademy/dogs/DogsApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.dogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
