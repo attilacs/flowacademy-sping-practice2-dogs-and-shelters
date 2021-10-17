6725e03c74ca34b5aece1439acd2af14b760387e
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-17 08:01:56 +0200

    Add checkIfAddressAlreadyExists method

--- src/main/java/hu/flowacademy/dogs/services/ShelterService.java
+++ src/main/java/hu/flowacademy/dogs/services/ShelterService.java
@@ -2,6 +2,7 @@ package hu.flowacademy.dogs.services;
 
 import hu.flowacademy.dogs.dtos.ShelterDTO;
 import hu.flowacademy.dogs.entities.Shelter;
+import hu.flowacademy.dogs.exceptions.DuplicateAddressException;
 import hu.flowacademy.dogs.repositories.ShelterRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,15 @@ public class ShelterService {
         shelter.setDogs(new ArrayList<>());
         shelterRepository.save(shelter);
         return shelter;
+    }
+
+    private void checkIfAddressAlreadyExists(String address) {
+        if (shelterRepository
+                .findAll()
+                .stream()
+                .anyMatch(shelter -> shelter.getAddress().equalsIgnoreCase(address))) {
+            throw new DuplicateAddressException();
+        }
     }
 
 }
