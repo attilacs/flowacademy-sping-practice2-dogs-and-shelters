dda88401cbce8fa6b658049346a6337794bcdc13
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-17 08:07:59 +0200

    Add addressAlreadyExists to ErrorController

--- src/main/java/hu/flowacademy/dogs/controllers/ErrorController.java
+++ src/main/java/hu/flowacademy/dogs/controllers/ErrorController.java
@@ -1,5 +1,6 @@
 package hu.flowacademy.dogs.controllers;
 
+import hu.flowacademy.dogs.exceptions.AddressAlreadyExistsException;
 import hu.flowacademy.dogs.models.ErrorModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,5 +15,11 @@ public class ErrorController {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorModel notValidArgument() {
         return new ErrorModel("Érvénytelen bemenet!");
+    }
+
+    @ExceptionHandler({AddressAlreadyExistsException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorModel addressAlreadyExists() {
+        return new ErrorModel("A megadott cím már létezik");
     }
 }
