741f9d40d1d8142a7a07f83e92ac0e90c7d88318
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-17 12:38:27 +0200

    Modify addDog return type to DogResponse

--- src/main/java/hu/flowacademy/dogs/services/DogService.java
+++ src/main/java/hu/flowacademy/dogs/services/DogService.java
@@ -14,14 +14,15 @@ public class DogService {
     private final DogRepository dogRepository;
     private final ShelterService shelterService;
 
-    public Dog addDog(DogDTO dogDTO) {
+    public DogResponse addDog(DogDTO dogDTO) {
         Shelter shelter = shelterService.findShelterById(dogDTO.getShelter_id());
         Dog dog = new Dog();
         dog.setChipId(dogDTO.getChipId());
         dog.setName(dogDTO.getName());
         dog.setGender(dogDTO.getGender());
         dog.setShelter(shelter);
-        return dogRepository.save(dog);
+        dogRepository.save(dog);
+        return dogToDogResponse(dog);
     }
 
     public DogResponse dogToDogResponse(Dog dog){
