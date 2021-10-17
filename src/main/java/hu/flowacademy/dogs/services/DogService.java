8d3e44e5f982570a02f0fa20518fdfd6f5b86c58
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-17 12:03:02 +0200

    Add dogToDogResponse method to DogService

--- src/main/java/hu/flowacademy/dogs/services/DogService.java
+++ src/main/java/hu/flowacademy/dogs/services/DogService.java
@@ -1,6 +1,7 @@
 package hu.flowacademy.dogs.services;
 
 import hu.flowacademy.dogs.dtos.DogDTO;
+import hu.flowacademy.dogs.dtos.DogResponse;
 import hu.flowacademy.dogs.entities.Dog;
 import hu.flowacademy.dogs.entities.Shelter;
 import hu.flowacademy.dogs.repositories.DogRepository;
@@ -21,5 +22,15 @@ public class DogService {
         dog.setGender(dogDTO.getGender());
         dog.setShelter(shelter);
         return dogRepository.save(dog);
+    }
+
+    public DogResponse dogToDogResponse(Dog dog){
+        DogResponse dogResponse = new DogResponse();
+        dogResponse.setId(dog.getId());
+        dogResponse.setChipId(dog.getChipId());
+        dogResponse.setName(dog.getName());
+        dogResponse.setGender(dog.getGender());
+        dogResponse.setShelter_id(dog.getShelter().getId());
+        return dogResponse;
     }
 }
