f0131f121c54a0afea8f5d57c8489371a0e7934e
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-17 09:37:04 +0200

    Add getAllShelters method to ShelterController

--- src/main/java/hu/flowacademy/dogs/controllers/ShelterController.java
+++ src/main/java/hu/flowacademy/dogs/controllers/ShelterController.java
@@ -2,13 +2,13 @@ package hu.flowacademy.dogs.controllers;
 
 import hu.flowacademy.dogs.dtos.ShelterDTO;
 import hu.flowacademy.dogs.dtos.ShelterResponse;
-import hu.flowacademy.dogs.entities.Shelter;
 import hu.flowacademy.dogs.services.ShelterService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/shelter")
@@ -20,5 +20,10 @@ public class ShelterController {
     @ResponseStatus(HttpStatus.CREATED)
     public ShelterResponse addShelter(@RequestBody @Valid ShelterDTO shelterDTO) {
         return shelterService.addShelter(shelterDTO);
+    }
+
+    @GetMapping
+    public List<ShelterResponse> getAllShelters() {
+        return shelterService.getAllShelters();
     }
 }
