ed8c56b68618dcf273df3efe114887a606857c09
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-17 07:30:45 +0200

    Add addShelter method to ShelterController

--- src/main/java/hu/flowacademy/dogs/controllers/ShelterController.java
+++ src/main/java/hu/flowacademy/dogs/controllers/ShelterController.java
@@ -1,13 +1,21 @@
 package hu.flowacademy.dogs.controllers;
 
+import hu.flowacademy.dogs.dtos.ShelterDTO;
+import hu.flowacademy.dogs.entities.Shelter;
 import hu.flowacademy.dogs.services.ShelterService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/shelter")
 @AllArgsConstructor
 public class ShelterController {
     private final ShelterService shelterService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Shelter addShelter(@RequestBody ShelterDTO shelterDTO) {
+        return shelterService.addShelter(shelterDTO);
+    }
 }
