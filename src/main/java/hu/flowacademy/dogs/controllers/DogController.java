78daf00d544f5b57de401c3c1e515332e3707e44
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-17 12:40:51 +0200

    Add addDog method to DogController

--- src/main/java/hu/flowacademy/dogs/controllers/DogController.java
+++ src/main/java/hu/flowacademy/dogs/controllers/DogController.java
@@ -1,13 +1,21 @@
 package hu.flowacademy.dogs.controllers;
 
+import hu.flowacademy.dogs.dtos.DogDTO;
+import hu.flowacademy.dogs.dtos.DogResponse;
 import hu.flowacademy.dogs.services.DogService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/dog")
 @AllArgsConstructor
 public class DogController {
     private final DogService dogService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public DogResponse addDog(@RequestBody DogDTO dogDTO) {
+        return dogService.addDog(dogDTO);
+    }
 }
