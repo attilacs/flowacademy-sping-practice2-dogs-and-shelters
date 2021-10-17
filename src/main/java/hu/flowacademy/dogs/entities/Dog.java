c7446b8eb4f4d613f6b773909e03d7d923f14756
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-17 05:32:20 +0200

    Add Dog entity

---
+++ src/main/java/hu/flowacademy/dogs/entities/Dog.java
@@ -0,0 +1,23 @@
+package hu.flowacademy.dogs.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "dogs")
+@Data
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String chipId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String gender;
+}
