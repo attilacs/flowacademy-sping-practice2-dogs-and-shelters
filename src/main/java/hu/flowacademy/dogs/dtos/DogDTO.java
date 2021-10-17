b213279b3e7a66010eebb88e32d8833e90a0c371
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-17 07:15:53 +0200

    Add DogDTO

---
+++ src/main/java/hu/flowacademy/dogs/dtos/DogDTO.java
@@ -0,0 +1,23 @@
+package hu.flowacademy.dogs.dtos;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Data
+public class DogDTO {
+    @NotEmpty
+    @Pattern(regexp = "^[A-Z]{6}-[0-9]{2}]")
+    private String chipId;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String gender;
+
+    @NotNull
+    private Long shelter_id;
+}
