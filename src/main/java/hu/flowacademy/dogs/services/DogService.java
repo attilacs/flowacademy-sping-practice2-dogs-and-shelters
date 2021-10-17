48edbab4b57c145b0e93fb901e785e936b7d99cd
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-17 05:45:12 +0200

    Add DogService

---
+++ src/main/java/hu/flowacademy/dogs/services/DogService.java
@@ -0,0 +1,12 @@
+package hu.flowacademy.dogs.services;
+
+import hu.flowacademy.dogs.repositories.DogRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class DogService {
+
+    private final DogRepository dogRepository;
+}
