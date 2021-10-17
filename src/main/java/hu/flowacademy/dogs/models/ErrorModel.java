596ddfc0d89e6b516946db599bf5b2be01601589
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-17 07:19:18 +0200

    Add ErrorModel

---
+++ src/main/java/hu/flowacademy/dogs/models/ErrorModel.java
@@ -0,0 +1,12 @@
+package hu.flowacademy.dogs.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorModel {
+    private String message;
+}
