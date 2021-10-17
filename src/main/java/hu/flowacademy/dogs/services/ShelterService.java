577dea09a1a7e8fe99c4788830face2fd10527ea
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-17 07:27:34 +0200

    Add addShelter method to ShelterService

--- src/main/java/hu/flowacademy/dogs/services/ShelterService.java
+++ src/main/java/hu/flowacademy/dogs/services/ShelterService.java
@@ -1,5 +1,7 @@
 package hu.flowacademy.dogs.services;
 
+import hu.flowacademy.dogs.dtos.ShelterDTO;
+import hu.flowacademy.dogs.entities.Shelter;
 import hu.flowacademy.dogs.repositories.ShelterRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,5 +10,13 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class ShelterService {
     private final ShelterRepository shelterRepository;
+
+    public Shelter addShelter(ShelterDTO shelterDTO) {
+        Shelter shelter = new Shelter();
+        shelter.setAddress(shelter.getAddress());
+        shelter.setCapacity(shelterDTO.getCapacity());
+        shelterRepository.save(shelter);
+        return shelter;
+    }
 
 }
